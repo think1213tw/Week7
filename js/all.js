e9da0fcf9979511b259ddc6a69c1138e20873d6d
think1213tw <think1213tw@gmail.com>
2020-08-27 18:59:53 +0800

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+   
+    $('.collapse .toggler').click(function () {
+        $('.navbar-nav').toggleClass('d-flex')
+      
+      }
+  
+      )
+ 
+
+   
+
+
+
+
+});
+
+
+
+// $('#title').hide()隱藏效果 ; /.toggle 檢查開關/.slideToggle(2000<毫秒數 2秒)滑落滑出效果
+//.fadeToggle()淡入淡出效果
+//選擇器 先選擇一個東西再決定讓他做什麼樣的功能
+
+// $('p').slideUp(1000).slideDown(3000); 加上一個. 前面樣式先跑
+//$('p').toggleClass('orange') 動態加上css樣式 
+//$('a').attr('href','https://tw.yahoo.com/'); 動態變更a標籤網址
+// $('body').html('<h1>1234</h1>'); 動態載入1234
+//$('h1').click(function(event){});  當滑鼠點擊h1標籤 就觸發
+
+   
